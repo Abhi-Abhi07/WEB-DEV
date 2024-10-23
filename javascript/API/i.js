bdf816c986f985f9ec85a64d353fbeb6f5879310
Abhishek <abhigurjar572@gmail.com>
2024-10-23 12:17:34 +0530

    first upload

---
+++ javascript/API/i.js
@@ -0,0 +1,17 @@
+let cityName = document.querySelector(".weather_city");
+let dateTime = document.querySelector(".weather_date_time");
+let w_forecast = document.querySelector(".weather_forecast");
+let w_icon = document.querySelector(".weather_icon");
+let w_temperature = document.querySelector(".weather_temperature");
+let w_minTem = document.querySelector(".weather_min");
+let w_maxTem = document.querySelector(".weather_max");
+
+let w_feelsLike = document.querySelector(".weather_feelsLike");
+let w_humidity = document.querySelector(".weather_humidity");
+let w_wind = document.querySelector(".weather_wind");
+let w_pressure = document.querySelector(".weather_pressure");
+
+let citySearch = document.querySelector(".weather_search");
+
+let city="pune";
+
